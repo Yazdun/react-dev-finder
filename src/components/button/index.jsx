a0191416e48f86b031a09bbaa5e170b87d2397ce
Yazdun <yazduntube@gmail.com>
2022-05-31 13:01:01 +0430

    search bar : made progress

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,5 +1,5 @@
 import css from './styles.module.css'
 
-export const Button = () => {
-  return <div>Button</div>
+export const Button = ({ children }) => {
+  return <button className={css.btn}>{children}</button>
 }
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,5 +1,19 @@
 import css from './styles.module.css'
+import { Button } from '../'
+import { SearchSvg } from './SearchSvg'
 
 export const Search = () => {
-  return <div className={css.search}>Search</div>
+  return (
+    <div className={css.search}>
+      <div className={css.bar}>
+        <SearchSvg />
+        <input
+          className={css.input}
+          type="text"
+          placeholder="Search GitHub username..."
+        />
+      </div>
+      <Button>Search</Button>
+    </div>
+  )
 }
