6754fa18f11730b73d7f867f28e6fb85e727299b
Yazdun <yazduntube@gmail.com>
2022-05-31 22:23:17 +0430

    general : made progress

--- src/components/stats/index.jsx
+++ src/components/stats/index.jsx
@@ -1,21 +1,24 @@
 import css from './styles.module.css'
+import CountUp from 'react-countup'
 
 export const Stats = props => {
   const { followers, following, public_repos } = props
   return (
     <ul className={css.stats}>
-      <li className={css.item}>
-        <span className={css.title}>Repos</span>
-        <span className={css.num}>{public_repos}</span>
-      </li>
-      <li className={css.item}>
-        <span className={css.title}>Followers</span>
-        <span className={css.num}>{followers}</span>
-      </li>
-      <li className={css.item}>
-        <span className={css.title}>Following</span>
-        <span className={css.num}>{following}</span>
-      </li>
+      <Item title="Repos" count={public_repos} />
+      <Item title="Followers" count={followers} />
+      <Item title="Following" count={following} />
     </ul>
+  )
+}
+
+const Item = ({ title, count }) => {
+  return (
+    <li className={css.item}>
+      <span className={css.title}>{title}</span>
+      <span className={css.num}>
+        <CountUp start={0} end={count} delay={0.3} duration={0.8} />
+      </span>
+    </li>
   )
 }
