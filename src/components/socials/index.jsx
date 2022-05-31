0ead53f23181f1334e056b06b69bb8540c095664
Yazdun <yazduntube@gmail.com>
2022-05-31 17:06:34 +0430

    general : made progress

--- src/components/socials/index.jsx
+++ src/components/socials/index.jsx
@@ -1,5 +1,28 @@
 import css from './styles.module.css'
+import { IoLocationSharp, IoLogoTwitter } from 'react-icons/io5'
+import { RiBuilding2Fill } from 'react-icons/ri'
+import { BiLink } from 'react-icons/bi'
 
-export const Socials = () => {
-  return <div>Socials</div>
+export const Socials = props => {
+  const { location, twitter_username, company, blog } = props
+  return (
+    <ul className={css.list}>
+      <li className={css.item}>
+        <IoLocationSharp />
+        <span>{location}</span>
+      </li>
+      <li className={css.item}>
+        <IoLogoTwitter />
+        <span>@{twitter_username}</span>
+      </li>
+      <li className={css.item}>
+        <BiLink />
+        <span>{blog}</span>
+      </li>
+      <li className={css.item}>
+        <RiBuilding2Fill />
+        <span>@{company}</span>
+      </li>
+    </ul>
+  )
 }
