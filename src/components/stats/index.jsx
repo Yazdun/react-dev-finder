9e18c8bfbaeb285b95774d43536c6061f5ee95b8
Yazdun <yazduntube@gmail.com>
2022-05-31 16:09:28 +0430

    stats : completed

--- src/components/stats/index.jsx
+++ src/components/stats/index.jsx
@@ -1,5 +1,21 @@
 import css from './styles.module.css'
 
-export const Stats = () => {
-  return <div>Stats</div>
+export const Stats = props => {
+  const { followers, following, public_repos } = props
+  return (
+    <ul className={css.stats}>
+      <li className={css.item}>
+        <span className={css.title}>Repos</span>
+        <span className={css.num}>{public_repos}</span>
+      </li>
+      <li className={css.item}>
+        <span className={css.title}>Followers</span>
+        <span className={css.num}>{followers}</span>
+      </li>
+      <li className={css.item}>
+        <span className={css.title}>Following</span>
+        <span className={css.num}>{following}</span>
+      </li>
+    </ul>
+  )
 }
